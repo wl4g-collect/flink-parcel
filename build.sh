79b3f7aa9eb4ba7f3c8659f6a4abdac5f20a1a00
wl4g <983708408@qq.com>
2022-05-21 19:10:54 +0800

    feat: support flink 1.11.2 cdh6.3

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -x
+# for exception
 set -e
+## for debug.
 set -v
+set -x
 
 FLINK_URL=`sed '/^FLINK_URL=/!d;s/.*=//' flink-parcel.properties` 
 FLINK_VERSION=`sed '/^FLINK_VERSION=/!d;s/.*=//' flink-parcel.properties`
@@ -13,17 +15,18 @@ CDH_MIN=`sed '/^CDH_MIN=/!d;s/.*=//' flink-parcel.properties`
 CDH_MAX_FULL=`sed '/^CDH_MAX_FULL=/!d;s/.*=//' flink-parcel.properties`
 CDH_MAX=`sed '/^CDH_MAX=/!d;s/.*=//' flink-parcel.properties`
 
-flink_service_name="FLINK"
-flink_service_name_lower="$( echo $flink_service_name | tr '[:upper:]' '[:lower:]' )"
-flink_archive="$( basename $FLINK_URL )"
-flink_unzip_folder="${flink_service_name_lower}-${FLINK_VERSION}"
-flink_folder_lower="$( basename $flink_archive .tgz )"
-flink_parcel_folder="$( echo $flink_folder_lower | tr '[:lower:]' '[:upper:]')"
+base_dir="$(cd `dirname $0`/;pwd)/build"; mkdir -p ${base_dir}
+flink_service_name="flink"
+flink_service_name_lower="$(echo $flink_service_name | tr '[:upper:]' '[:lower:]')"
+flink_archive="${base_dir}/$(basename $FLINK_URL)"
+flink_unzip_folder="${base_dir}/${flink_service_name_lower}-${FLINK_VERSION}"
+flink_folder_lower="$(basename $flink_archive .tgz)"
+flink_parcel_folder="${base_dir}/$(echo $flink_folder_lower | tr '[:upper:]' '[:lower:]')"
 flink_parcel_name="$flink_parcel_folder-el${OS_VERSION}.parcel"
-flink_built_folder="${flink_parcel_folder}_build"
-flink_csd_build_folder="flink_csd_build"
+flink_built_folder="${base_dir}/${flink_parcel_folder}"
+flink_csd_build_folder="${base_dir}/flink_csd"
 
-function build_cm_ext {  #Checkout if dir does not exist
+function build_cm_ext {  # Checkout if dir does not exist
   if [ ! -d cm_ext ]; then
     git clone https://github.com/cloudera/cm_ext.git
   fi
@@ -45,7 +48,8 @@ function get_flink {
     exit 1
   fi
   if [ ! -d "$flink_unzip_foleder" ]; then
-    tar -xvf $flink_archive
+    mkdir -p ${flink_unzip_folder}
+    tar -xvf $flink_archive --strip-components=1 -C ${flink_unzip_folder}
   fi
 }
 
@@ -62,26 +66,28 @@ function build_flink_parcel {
   fi
   cp -r flink-parcel-src/meta $flink_parcel_folder/
   chmod 755 flink-parcel-src/flink*
-  cp -r flink-parcel-src/flink-master.sh ${flink_parcel_folder}/lib/${flink_service_name_lower}/bin/
-  cp -r flink-parcel-src/flink-worker.sh ${flink_parcel_folder}/lib/${flink_service_name_lower}/bin/
-  cp -r flink-parcel-src/flink-yarn.sh ${flink_parcel_folder}/lib/${flink_service_name_lower}/bin/
-  sed -i -e "s/%flink_version%/$flink_parcel_folder/" ./$flink_parcel_folder/meta/flink_env.sh
-  sed -i -e "s/%VERSION%/$FLINK_VERSION/" ./$flink_parcel_folder/meta/parcel.json
-  sed -i -e "s/%EXTENS_VERSION%/$EXTENS_VERSION/" ./$flink_parcel_folder/meta/parcel.json
-  sed -i -e "s/%CDH_MAX_FULL%/$CDH_MAX_FULL/" ./$flink_parcel_folder/meta/parcel.json
-  sed -i -e "s/%CDH_MIN_FULL%/$CDH_MIN_FULL/" ./$flink_parcel_folder/meta/parcel.json
-  sed -i -e "s/%SERVICENAME%/$flink_service_name/" ./$flink_parcel_folder/meta/parcel.json
-  sed -i -e "s/%SERVICENAMELOWER%/$flink_service_name_lower/" ./$flink_parcel_folder/meta/parcel.json
-  java -jar cm_ext/validator/target/validator.jar -d ./$flink_parcel_folder
+  local build_flink_parcel_folder="${flink_parcel_folder}/lib/${flink_service_name_lower}"
+  mkdir -p $build_flink_parcel_folder/bin
+  cp -r flink-parcel-src/flink-master.sh ${build_flink_parcel_folder}/bin
+  cp -r flink-parcel-src/flink-worker.sh ${build_flink_parcel_folder}/bin
+  cp -r flink-parcel-src/flink-yarn.sh ${build_flink_parcel_folder}/bin
+  sed -i -e "s#%flink_version%#$flink_parcel_folder#g" ${flink_parcel_folder}/meta/flink_env.sh
+  sed -i -e "s#%VERSION%#$FLINK_VERSION#g" ${flink_parcel_folder}/meta/parcel.json
+  sed -i -e "s#%EXTENS_VERSION%#$EXTENS_VERSION#g" ${flink_parcel_folder}/meta/parcel.json
+  sed -i -e "s#%CDH_MAX_FULL%#$CDH_MAX_FULL#g" ${flink_parcel_folder}/meta/parcel.json
+  sed -i -e "s#%CDH_MIN_FULL%#$CDH_MIN_FULL#g" ${flink_parcel_folder}/meta/parcel.json
+  sed -i -e "s#%SERVICENAME%#$flink_service_name#g" ${flink_parcel_folder}/meta/parcel.json
+  sed -i -e "s#%SERVICENAMELOWER%#$flink_service_name_lower#g" ${flink_parcel_folder}/meta/parcel.json
+  java -jar cm_ext/validator/target/validator.jar -d ${flink_parcel_folder}
   mkdir -p $flink_built_folder
-  tar zcvhf ./$flink_built_folder/$flink_parcel_name $flink_parcel_folder --owner=root --group=root
-  java -jar cm_ext/validator/target/validator.jar -f ./$flink_built_folder/$flink_parcel_name
-  python cm_ext/make_manifest/make_manifest.py ./$flink_built_folder
-  sha1sum ./$flink_built_folder/$flink_parcel_name |awk '{print $1}' > ./$flink_built_folder/${flink_parcel_name}.sha
+  tar zcvhf $flink_built_folder/$flink_parcel_name $flink_parcel_folder --owner=root --group=root
+  java -jar cm_ext/validator/target/validator.jar -f $flink_built_folder/$flink_parcel_name
+  python cm_ext/make_manifest/make_manifest.py $flink_built_folder
+  sha1sum $flink_built_folder/$flink_parcel_name |awk '{print $1}' > $flink_built_folder/${flink_parcel_name}.sha
 }
 
 function build_flink_csd_on_yarn {
-  JARNAME=${flink_service_name}_ON_YARN-${FLINK_VERSION}.jar
+  JARNAME=${flink_service_name}_on_yarn-${FLINK_VERSION}.jar
   if [ -f "$JARNAME" ]; then
     return
   fi
@@ -129,4 +135,3 @@ csd_standalone)
   echo "Usage: $0 [parcel|csd_on_yarn|csd_standalone]"
   ;;
 esac
-
